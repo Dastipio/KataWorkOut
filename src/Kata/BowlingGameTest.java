f8b8f08f8aee0fe730f7c0320ecd34251cf0212d
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-05-15 00:07:59 +0200

    test oneSpare Red

--- src/Kata/BowlingGameTest.java
+++ src/Kata/BowlingGameTest.java
@@ -5,10 +5,25 @@ import Kata.BowlingGame;
 import org.junit.Before;
 import org.junit.Test;
 
+
+//STRIKE – zbijasz wszystkie krêgle w pierwszym rzucie . Kolejne Twoje rzuty w nastêpnych kolejkach liczone s¹ 2 razy, w 10 kolejce razy1.
+//
+//SPARE – zbijasz wszystkie krêgle w kolejce w dwóch rzutach . W nastêpnej kolejce pierwszy rzut liczony jest razy 2, w 10 kolejce razy 1.
+//
+//SPLIT – po pierwszym rzucie pozostajesz ze skrajnie ustawionymi krêglami, z których mo¿esz zbiæ tylko 1.
+//
+//FOUL – przekroczy³eœ liniê rzutu – spali³eœ, je¿eli zbi³eœ krêgle, punkty nie s¹ liczone, ale rzut tak
+
 public class BowlingGameTest {
 
 	private BowlingGame game;
 
+	private void rollMany(int rolls, int pins) {
+		for (int i = 0; i < rolls; i++) {
+			game.roll(pins);
+		}
+	}
+	
 	@Test
 	public void canCreateGame() throws Exception {
 		setUp();
@@ -19,17 +34,14 @@ public class BowlingGameTest {
 		game.roll(0); 
 	}
 	
-	private void rollMany(int rolls, int pins) {
-		for (int i = 0; i < rolls; i++) {
-			game.roll(pins);
-		}
-	}
+	
 
 	@Before
 	public void setUp() throws Exception {
 		game = new BowlingGame();
 	}
 
+	
 	@Test
 	public void gutterGame_ShouldReturnRed() {
 		rollMany(20, 0);
@@ -37,13 +49,22 @@ public class BowlingGameTest {
 	}
 
 	
-
 	@Test
 	public void allOnes() {
 		for (int i = 0; i < 20; i++) {
 			game.roll(1);
 		}
 		assertEquals(20, game.score());
+	}
+	
+	@Test
+	public void oneSpare(){
+		game.roll(5);
+		game.roll(5); //spare
+		game.roll(3);
+		rollMany(17, 0);
+		
+		assertEquals(16, game.score());
 	}
 
 }
