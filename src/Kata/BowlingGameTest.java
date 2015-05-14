a12bb0b0c166690477076ad1ec86b3ef7eb3f2de
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-05-14 23:42:34 +0200

    Dodanie metody setUp

--- src/Kata/BowlingGameTest.java
+++ src/Kata/BowlingGameTest.java
@@ -1,20 +1,31 @@
 package Kata;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class BowlingGameTest {
 
+	private BowlingGame game;
+	
 	@Test
-	public void canCreateGame(){
-		BowlingGame game = new BowlingGame();
+	public void canCreateGame() throws Exception{
+		setUp();
 	}
 	
 	@Test
 	public void canRoll(){
-		BowlingGame game = new BowlingGame();
 		game.roll(0);
 	
 	}
+
+	@Before
+	public void setUp() throws Exception {
+			game = new BowlingGame();
+		
+	}
+	
+	
 }
