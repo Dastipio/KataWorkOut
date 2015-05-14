72959e73774b22ee4ee01d18b366e67986236428
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-05-14 23:27:02 +0200

    Utworzenie BowlingGameTest

---
+++ src/Kata/BowlingGameTest.java
@@ -0,0 +1,14 @@
+package Kata;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class BowlingGameTest {
+
+	@Test
+	public void canCreateGame(){
+		BowlingGame game = new BowlingGame();
+		
+	
+	}
+}
